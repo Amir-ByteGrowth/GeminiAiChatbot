d9cac74f3082747e9dabcdb313d5d808d5088b99
Amir javeed <amir@vaultspay.ae>
2024-06-19 16:38:54 +0400

    suggestion model added for suggestion implementation in chat screen

---
+++ app/src/main/java/com/example/geminiaichatbot/models/SuggestionsModel.kt
@@ -0,0 +1,31 @@
+package com.example.geminiaichatbot.models
+
+data class SuggestionsModel(val text: String)
+
+
+var availableSuggestions = listOf(
+    "abc",
+    "abcd",
+    "abcde",
+    "cde",
+    "cdef",
+    "cdefg",
+    "fgh",
+    "fghi",
+    "fghij",
+    "fghijk",
+    "jkl",
+    "jklm",
+    "jklmn",
+    "lmn",
+    "lmnop",
+    "lmnopqr",
+    "op",
+    "opq",
+    "opqr",
+    "opqrs",
+    "opqrst",
+    "stuvw",
+    "stab",
+    "bsxy"
+)
